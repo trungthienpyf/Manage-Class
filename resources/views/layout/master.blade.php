9689deb19b58a4ad4a7fc0222e89c0afe31fb5b0
Nguyen Trung Thien <269.tdd@gmail.com>
2022-09-23 23:08:05 +0700

    update ui

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,37 @@
+<!-- start page title -->
+<div class="row">
+    <div class="col-12">
+        <div class="page-title-box">
+            <div class="page-title-right">
+                <ol class="breadcrumb m-0">
+                    <li class="breadcrumb-item"><a href="javascript: void(0);">Hyper</a></li>
+                    <li class="breadcrumb-item"><a href="javascript: void(0);">Layout</a></li>
+                    <li class="breadcrumb-item active">Detached</li>
+                </ol>
+            </div>
+            <h4 class="page-title">Detached Layout</h4>
+        </div>
+    </div>
+</div>
+<!-- end page title -->
+
+<div class="row">
+
+    <!-- end col -->
+
+    <!-- end col -->
+            @yield('content')
+    <!-- end col -->
+
+    <!-- end col -->
+</div>
+<!-- end row -->
+
+
+<!-- end row-->
+
+
+
+<!-- end row-->
+
+</div> <!-- End Content -->
