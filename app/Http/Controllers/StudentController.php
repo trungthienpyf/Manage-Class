00d3719da677b7d43ccb49f6fce6cf9bf5549460
Nguyen Trung Thien <269.tdd@gmail.com>
2022-10-25 22:19:30 +0700

    wip(student): register subject

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 
 use App\Models\ClassSchedule;
+use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
@@ -26,6 +27,12 @@ class StudentController extends Controller
         return view('student.progress');
     }
 
+    public function resultPayment(Request $request)
+    {
+        dd($request->all());
+        return view('student.progress');
+    }
+
     public function paymentQR()
     {
         $endpoint = "https://test-payment.momo.vn/v2/gateway/api/create";
@@ -37,43 +44,40 @@ class StudentController extends Controller
 
         $orderInfo = "Thanh toán qua QR MoMo";
         $amount = "10000";
-        $orderId = time() ."";
-        $redirectUrl = "https://webhook.site/b3088a6a-2d17-4f8d-a383-71389a6c600b";
-        $ipnUrl = "https://webhook.site/b3088a6a-2d17-4f8d-a383-71389a6c600b";
+        $orderId = time() . "";
+        $redirectUrl = route('resultPayment');
+        $ipnUrl = route('resultPayment');
         $extraData = "";
 
 
+        $requestId = time() . "";
+        $requestType = "captureWallet";
+
+        //before sign HMAC SHA256 signature
+        $rawHash = "accessKey=" . $accessKey . "&amount=" . $amount . "&extraData=" . $extraData . "&ipnUrl=" . $ipnUrl . "&orderId=" . $orderId . "&orderInfo=" . $orderInfo . "&partnerCode=" . $partnerCode . "&redirectUrl=" . $redirectUrl . "&requestId=" . $requestId . "&requestType=" . $requestType;
+        $signature = hash_hmac("sha256", $rawHash, $secretKey);
+        $data = array('partnerCode' => $partnerCode,
+            'partnerName' => "Test",
+            "storeId" => "MomoTestStore",
+            'requestId' => $requestId,
+            'amount' => $amount,
+            'orderId' => $orderId,
+            'orderInfo' => $orderInfo,
+            'redirectUrl' => $redirectUrl,
+            'ipnUrl' => $ipnUrl,
+            'lang' => 'vi',
+            'extraData' => $extraData,
+            'requestType' => $requestType,
+            'signature' => $signature);
+        $result = $this->execPostRequest($endpoint, json_encode($data));
+        $jsonResult = json_decode($result, true);  // decode json
 
 
-
-
-            $requestId = time() . "";
-            $requestType = "captureWallet";
-
-            //before sign HMAC SHA256 signature
-            $rawHash = "accessKey=" . $accessKey . "&amount=" . $amount . "&extraData=" . $extraData . "&ipnUrl=" . $ipnUrl . "&orderId=" . $orderId . "&orderInfo=" . $orderInfo . "&partnerCode=" . $partnerCode . "&redirectUrl=" . $redirectUrl . "&requestId=" . $requestId . "&requestType=" . $requestType;
-            $signature = hash_hmac("sha256", $rawHash, $secretKey);
-            $data = array('partnerCode' => $partnerCode,
-                'partnerName' => "Test",
-                "storeId" => "MomoTestStore",
-                'requestId' => $requestId,
-                'amount' => $amount,
-                'orderId' => $orderId,
-                'orderInfo' => $orderInfo,
-                'redirectUrl' => $redirectUrl,
-                'ipnUrl' => $ipnUrl,
-                'lang' => 'vi',
-                'extraData' => $extraData,
-                'requestType' => $requestType,
-                'signature' => $signature);
-            $result = $this->execPostRequest($endpoint, json_encode($data));
-            $jsonResult = json_decode($result, true);  // decode json
-
-
-return $jsonResult;
-          // return redirect($jsonResult['payUrl']);
+        return $jsonResult;
+       // return redirect($jsonResult['payUrl']);
 
     }
+
     function execPostRequest($url, $data)
     {
         $ch = curl_init($url);
